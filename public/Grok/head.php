034199c8f21e05439a62a241c186fee90053df25
Nahuel <nbaste@gmail.com>
2021-11-30 23:29:22 -0300

    Added Grok
    Replaced png for jpg img references

---
+++ public/Grok/head.php
@@ -0,0 +1,64 @@
+<title>Nahuel Basterretche - Designer & Developer</title>
+    <meta name="title" content="Nahuel Basterretche - Designer & Developer">
+    <meta name="description"
+        content="Nahbaste is Nahuel Basterretche, an XR designer and developer from Buenos Aires. He specializes in Instagram Filters and 3D interactive experiences">
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || []; w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            }); var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-T5Q6B7D');</script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.nahbaste.com/">
+    <meta property="og:title" content="Nahuel Basterretche - Designer & Developer">
+    <meta property="og:description"
+        content="Nahbaste is Nahuel Basterretche, an XR designer and developer from Buenos Aires. He specializes in Instagram Filters and 3D interactive experiences.">
+    <meta property="og:image" content="https://wwww.nahbaste.com/metatags.png">
+    <meta property="fb:app_id" content="950828529106312" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://www.nahbaste.com/">
+    <meta property="twitter:title" content="Nahuel Basterretche - Designer & Developer">
+    <meta property="twitter:description"
+        content="Nahbaste is Nahuel Basterretche, an XR designer and developer from Buenos Aires. He specializes in Instagram Filters and 3D interactive experiences.">
+    <meta property="twitter:image" content="https://www.nahbaste.com/metatags.png">
+
+
+    <link rel="canonical" href="https://www.nahbaste.com/" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <meta name="robots" content="index, follow">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="../assets/css/main2.css" />
+    <noscript>
+        <link rel="stylesheet" href="../assets/css/noscript.css" />
+    </noscript>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- FavIcons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="../apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+    <link rel="manifest" href="../manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
---
+++ public/Grok/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+<?php
+    include(‘head.php’);
+?>
+</head>
+
+<body class="landing is-preload">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5Q6B7D" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <header id="header" class="alt">
+            <h1><a href="../index.html">Nahuel Basterretche</a></h1>
+            <?php
+                include(‘nav.php’);
+            ?>
+        </header>
+
+        <!-- Banner -->
+        <section id="banner" class="small">
+            <div class="inner">
+
+                <a href="../index.html"> <img src="../assets/logosmall.svg" alt="nahbaste-logo"
+                        class="nahbasteLogo"></a>
+                <div id="main">
+                    <div id="section">
+                        <a href="index.html">
+                            <h1 id="section5" class="text-focus-in">Work</h1>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Main -->
+
+        <article id="main">
+            <section class="wrapper style5">
+                <div class="inner">
+                    <!-- <a href="../index.html"> <img src="../assets/logosmall.svg" alt="nahbaste-logo" class="nahbasteLogo"></a> -->
+                    <section>
+                        <header>
+                            <h4>Grok</h4>
+                            <p>Public bicycles system with gamification mechanics. </p>
+                        </header>
+                    </section>
+                </div>
+            </section>
+        </article>
+
+
+
+
+
+        <!-- Footer -->
+
+        <footer id="footer">
+            <ul class="icons">
+                <li><a href="https://www.linkedin.com/in/nahbaste/" target=”_blank”
+                        class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                <li><a href="https://www.behance.net/nahbaste" target=”_blank” class="icon brands fa-behance"><span
+                            class="label">Behance</span></a></li>
+                <li><a href="https://www.instagram.com/nahbaste" target=”_blank” class="icon brands fa-instagram"><span
+                            class="label">Instagram</span></a></li>
+                <li><a href="#" target=”_blank” class="icon brands fa-github"><span class="label">Github</span></a>
+                </li>
+                <li><a href="mailto:contact@nahbaste.com" target=”_blank” class="icon solid fa-envelope"><span
+                            class="label">Email</span></a>
+                </li>
+            </ul>
+            <ul class="copyright">
+                <li>&copy; nahbaste</li>
+                <li>Designed & developped by Nahuel Basterretche using some <a
+                        href="/About/index.html#useful-resources">useful resources</a></li>
+            </ul>
+        </footer>
+
+    </div>
+
+    <!-- Scripts -->
+
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/jquery.scrollex.min.js"></script>
+    <script src="../assets/js/jquery.scrolly.min.js"></script>
+    <script src="../assets/js/browser.min.js"></script>
+    <script src="../assets/js/breakpoints.min.js"></script>
+    <script src="../assets/js/util.js"></script>
+    <script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
